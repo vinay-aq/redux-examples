7cb824d259be25f7fb0d671d57bc6057e800353b
vinay-aq <vinay.naugain@airlinq.com>
2022-04-17 17:21:31 +0530

    remove redundant code arrray

--- shopping-cart/src/components/App.js
+++ shopping-cart/src/components/App.js
@@ -6,11 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { addProductToCart, reduceProductFromInventory } from "../redux";
 
 function App() {
-  let initialAvailableProducts = [
-    { id: 1, title: "iPad 4 Mini", price: 500, inventory: 2 },
-    { id: 2, title: "H&M T-Shirt White", price: 10, inventory: 10 },
-    { id: 3, title: "Charli XCX - Sucker CD", price: 20, inventory: 5 },
-  ];
+
   let initialCartProducts = [];
   // {"id": 1, "title": "iPad 4 Mini", "price": 500, "purchasedQty": 2},
 
